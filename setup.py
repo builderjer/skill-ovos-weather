e275b90fc0738d4677586e207e39bbd84660e857
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-10-11 18:56:15 +0100

    fix imports (#12)

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from setuptools import setup
-from os import path, walk
+import os
+from os import walk, path
+
 
 URL = "https://github.com/OpenVoiceOS/skill-ovos-weather"
 SKILL_CLAZZ = "WeatherSkill"  # needs to match __init__.py class name
